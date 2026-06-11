86e7e2541a61f40d5cff7eeca48d3fb200d3c124
jhonatanPena <jhonatanpenamora@gmail.com>
2026-06-11 12:52:51 -0500

    Solución Ejercicio2

---
+++ src/DOSW/Semana1/streams/Ejercicio2.java
@@ -0,0 +1,18 @@
+package DOSW.Semana1.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        List<String> palabras = Arrays.asList("java", "stream", "api", "functional", "code", "git");
+
+        long cantidad = palabras.stream()
+                .filter(p -> p.length() > 4)
+                .map(String::toUpperCase)
+                .sorted()
+                .count();
+
+        System.out.println("Cantidad de palabras resultantes: " + cantidad);
+    }
+}
