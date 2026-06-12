bd74b52163911dd6158d0e1d4252dc4a1937438c
jhonatanPena <jhonatanpenamora@gmail.com>
2026-06-11 22:15:28 -0500

    Refactor Clase Pokemon

--- src/DOSW/Semana2/pokemon/Pokemon.java
+++ src/DOSW/Semana2/pokemon/Pokemon.java
@@ -1,9 +1,24 @@
 package DOSW.Semana2.pokemon;
 
 public class Pokemon {
+    private Long id;
     private String nombre;
     private String tipo;
     private int nivel;
+    private double poderCombate;
+    private String region;
+    private boolean legendario;
+    private boolean puedeEvolucionar;
+
+    public Pokemon(Long id, String nombre, String tipo, int nivel, double poderCombate, String region, boolean legendario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.nivel = nivel;
+        this.poderCombate = poderCombate;
+        this.region = region;
+        this.legendario = legendario;
+    }
 
     public Pokemon(String nombre, String tipo) {
         this.nombre = nombre;
@@ -13,6 +28,15 @@ public class Pokemon {
     public Pokemon(String nombre, int nivel) {
         this.nombre = nombre;
         this.nivel = nivel;
+    }
+
+    public Pokemon(String nombre, boolean puedeEvolucionar) {
+        this.nombre = nombre;
+        this.puedeEvolucionar = puedeEvolucionar;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNombre() {
@@ -26,5 +50,20 @@ public class Pokemon {
     public int getNivel() {
         return nivel;
     }
-}
 
+    public double getPoderCombate() {
+        return poderCombate;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public boolean isLegendario() {
+        return legendario;
+    }
+
+    public boolean isPuedeEvolucionar() {
+        return puedeEvolucionar;
+    }
+}
